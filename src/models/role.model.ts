134d1da1f1a35dc73f1dbc5d5e969df2354868a3
Brandon Castillo <brajoel98@gmail.com>
2023-10-27 15:30:18 -0700

    feat(app): :seedling: create seed of roles

---
+++ src/models/role.model.ts
@@ -0,0 +1,52 @@
+import { DataTypes, Model, Sequelize } from 'sequelize';
+import sequelizeConfig from '../config/sequelize.config';
+
+class Role extends Model {
+  public id!: number;
+
+  public name!: string;
+
+  public readonly createdAt!: Date;
+
+  public readonly updatedAt!: Date;
+
+  public readonly deletedAt!: Date;
+
+  static initialize(sequelize: Sequelize) {
+    this.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: new DataTypes.STRING(128),
+          allowNull: false,
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        deletedAt: {
+          type: DataTypes.DATE,
+          allowNull: true,
+        },
+      },
+      {
+        tableName: 'roles',
+        sequelize,
+      },
+    );
+  }
+}
+
+Role.initialize(sequelizeConfig);
+
+export default Role;
